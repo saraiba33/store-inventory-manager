fea9fb371ab9261bcf62f0248430e181700f93b1
Sara Ibarra <saraiba33@gmail.com>
2022-04-28 10:40:31 -0600

    Add hardcoded items to js file

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
 const form = document.querySelector("form")
 const table = document.querySelector("table")
+const clear = document.querySelector(".clear-button")
 
 
+let stock = [{
+    item: "Dexterity Vest",
+    sellIn: 10,
+    quality: 20,
+    catagory: "none"
+}, {
+    item: "Aged Brie",
+    sellIn: 2,
+    quality: 0,
+    catagory: "aged Brie"
+}, {
+    item: "Elixir of the Mongoose",
+    sellIn: 5,
+    quality: 7,
+    catagory: "none"
+}, {
+    item: "Sulfuras",
+    sellIn: 0,
+    quality: 80,
+    catagory: "sulfuras"
+}, {
+    item: "Backstage passes to a TAFKAL80ETC concert",
+    sellIn: 15,
+    quality: 20,
+    catagory: "backstage pass"
+}, ]
 
+
+stock.map(item => {
+    const tr = document.createElement("tr")
+    tr.classList.add("preset-rows")
+    tr.innerHTML = `
+    <td>${item.item}</td>
+    <td>Sell in ${item.sellIn} days</td>
+    <td>${item.quality}</td>
+    `
+    table.append(tr)
+})
 
 const encodedItems = localStorage.getItem("allItemsAdded");
 if (encodedItems) {
@@ -13,18 +51,19 @@ if (encodedItems) {
         createTr.classList.add("added-rows")
         createTr.innerHTML = `
         <td>${items.item}</td>
-        <td>${items.sellIn}</td>
+        <td>Sell in ${items.sellIn} days</td>
         <td>${items.quality}</td>
         <td>${items.date}</td>
         `
         table.append(createTr)
+
     })
 }
-
 
 function getInput() {
     form.addEventListener("submit", event => {
         event.preventDefault()
+        location.reload();
         const formData = new FormData(event.target)
         const itemName = formData.get("item-name")
         const sellIn = formData.get("sell-in")
@@ -37,6 +76,7 @@ function getInput() {
             date: date
         }
 
+
         const encodedItems = localStorage.getItem("allItemsAdded");
         const allItemsAdded = encodedItems ? JSON.parse(encodedItems).allItemsAdded : []
         allItemsAdded.push(newItemInfoAdded)
@@ -47,3 +87,8 @@ function getInput() {
 
 }
 getInput()
+
+clear.addEventListener("click", () => {
+    localStorage.clear()
+    location.reload();
+})
