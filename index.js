9c2171733b2f42a6bce79f53da60f54722ef81fa
Sara Ibarra <saraiba33@gmail.com>
2022-04-28 18:40:07 -0600

    Add error message

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const table = document.querySelector("table")
 const clear = document.querySelector(".clear-button")
 const nextDayButton = document.querySelector(".next-day")
 const previousDayButton = document.querySelector(".previous-day")
+const placeholder = document.querySelector(".placeholder")
+
 
 function getInput() {
     form.addEventListener("submit", event => {
@@ -36,20 +38,44 @@ if (encodedItems) {
     allItemsAdded.map((items) => {
         const createTr = document.createElement("tr")
         createTr.classList.add("added-rows")
-        if (items.item.includes("Sulfuras")) {
+        if (items.item.toLowerCase().includes("Sulfuras")) {
             createTr.innerHTML = `
-        <td>${items.item}</td>
-        <td class="sell-in-days">N/A</td>
-        <td class="item-quality">80</td>
-        `
+            <td>${items.item}</td>
+            <td class="sell-in-days">N/A</td>
+            <td class="item-quality">80</td>
+            `
             table.append(createTr)
+            placeholder.remove()
+            return errorMessage("")
+        } else if (items.item.includes("sulfuras")) {
+            createTr.innerHTML = `
+            <td>${items.item}</td>
+            <td class="sell-in-days">N/A</td>
+            <td class="item-quality">80</td>
+            `
+            table.append(createTr)
+            placeholder.remove()
+            return errorMessage("")
+        } else if (items.item.includes("/")) {
+            return errorMessage("** You can not add more than one item at a time **")
+        } else if (items.quality > 50) {
+            createTr.innerHTML = `
+            <td>${items.item}</td>
+            <td class="sell-in-days"> Sell in ${items.sellIn} days</td>
+            <td class="item-quality">50</td>
+            `
+            table.append(createTr)
+            placeholder.remove()
+            return errorMessage("** Quality max is 50 **")
         } else {
             createTr.innerHTML = `
-        <td>${items.item}</td>
-        <td class="sell-in-days">Sell in ${items.sellIn} days</td>
-        <td class="item-quality">${items.quality}</td>
-        `
+            <td>${items.item}</td>
+            <td class="sell-in-days">Sell in ${items.sellIn} days</td>
+            <td class="item-quality">${items.quality}</td>
+            `
             table.append(createTr)
+            placeholder.remove()
+            return errorMessage("")
         }
     })
 }
@@ -69,3 +95,9 @@ clear.addEventListener("click", () => {
     localStorage.clear()
     location.reload();
 })
+
+function errorMessage(message) {
+    const error = document.querySelector(".error")
+    error.style.color = "red"
+    error.textContent = message
+}
